9e4fef2e5a51289c3fb7fe67c93faa13081efbcd
manumohan2025 <manu.mohan2025@gmail.com>
2021-04-24 16:12:58 -0400

    Create vars.tf

---
+++ terraform-frontend/vars.tf
@@ -0,0 +1,6 @@
+variable "PROJECT_NAME "   {}
+variable "ENV"             {}
+variable INSTANCE_TYPE     {}
+variable "COMPONENT"       {}
+variable "bucket"          {}
+variable "region"          {}
